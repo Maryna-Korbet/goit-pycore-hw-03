72e47e0d4bae485de7f727622fe7fda7f6132fb7
Maryna-Korbet <m22v86k@gmail.com>
2025-01-01 16:57:47 -0500

    task 1

---
+++ task1.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+def get_days_from_today(date: str) -> int:
+
+    try:
+        target_date = datetime.strptime(date, '%Y-%m-%d').date()
+        current_date = datetime.today().date()
+        difference = (target_date - current_date).days
+        return difference
+    except Exception as e:
+        print(f"Date processing error: {e}")
+        return 0
+    
+
+print (f"Difference in days: {get_days_from_today("2021-10-09")}");
+
+
+
+
